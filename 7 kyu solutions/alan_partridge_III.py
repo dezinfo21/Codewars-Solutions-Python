e5457baa92a7c990a9d52d247dea87f8be61b797
dezinfo21 <73795795+dezinfo21@users.noreply.github.com>
2021-06-29 23:06:35 +0300

    Create alan_partridge_III.py

---
+++ 7 kyu solutions/alan_partridge_III.py
@@ -0,0 +1,3 @@
+def alan(arr):
+    s = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway']
+    return "Smell my cheese you mother!" if all(x in arr for x in s) else "No, seriously, run. You will miss it."
